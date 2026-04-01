689278f1f8bebc4f9fa59266b50dcdaa17558e47
Pramanik26458 <basakpramanik9@gmail.com>
2026-04-01 18:53:10 +0530

    Fix: correct Home import path

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar       from "./components/Navbar";
-import Home         from "./pages/Home";
+import Home         from "./Pages/Home";
 import AllProduct   from "./Pages/AllProduct";
 import Men          from "./Pages/Men";
 import Women        from "./Pages/Women";
